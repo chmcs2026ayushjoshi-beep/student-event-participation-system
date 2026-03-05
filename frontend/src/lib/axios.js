93a0b0c966dcc82367c2957679710d0739070fb0
chmcs2026ayushjoshi-beep <chmcs2026.ayush.joshi@gmail.com>
2026-03-05 16:10:26 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 const api = axios.create({
-    baseURL: `https://student-event-participation-system.onrender.com/participants`
+    baseURL: `https://student-event-participation-system.onrender.com/`
 })
 export default api
